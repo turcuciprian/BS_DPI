12c40e462f8d3b9c0c836bb8ce88509975f7c10d
turcuciprian <turcuciprian1@gmail.com>
2023-05-21 09:57:38 +0300

    added resize script (not tested yet)

---
+++ resize.py
@@ -0,0 +1,44 @@
+import os
+from PIL import Image
+
+def resize_and_crop_images(input_path, output_path, size):
+    # Create output folder if it doesn't exist
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # Get a list of input files
+    files = os.listdir(input_path)
+
+    for file_name in files:
+        # Create input and output file paths
+        input_file = os.path.join(input_path, file_name)
+        output_file = os.path.join(output_path, file_name)
+
+        # Open the input image
+        image = Image.open(input_file)
+
+        # Resize and crop the image
+        resized_image = image.resize(size, Image.ANTIALIAS)
+
+        # Save the resized and cropped image
+        resized_image.save(output_file)
+
+    print("Images resized and cropped successfully.")
+
+#
+# Usage example:
+# 
+
+# input_folder = "path/to/input/folder"
+# output_folder = "path/to/output/folder"
+# image_size = (300, 200)
+
+# resize_and_crop_images(input_folder, output_folder, image_size)
+
+# using it:
+
+input_folder = "./before/resize"
+output_folder = "./after/resize"
+image_size = (800, 600)
+
+resize_and_crop_images(input_folder, output_folder, image_size)
